aa12050329d3ac0554e76b2c95e61fc032adcea0
Sphax <itssphax123@gmail.com>
2024-01-12 15:07:09 -0800

    Basic command line interface

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""command interpreter"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command class"""
+    prompt = "(hbnb) "
+    def do_quit(self, line):
+        """Quit command to exit the program
+        """
+        return True
+    def do_EOF(self, line):
+        """EOF exit the program
+        """
+        print("")
+        return True
+    def emptyline(self):
+        """Ignore empty lines
+        """
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
