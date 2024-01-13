ca29a45d86979b49a3a07227ea73ec42cf21baf1
Sphax <itssphax123@gmail.com>
2024-01-13 07:56:21 -0800

    removed tests

--- console.py
+++ console.py
@@ -6,19 +6,23 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """Command class"""
     prompt = "(hbnb) "
+
     def do_quit(self, line):
         """Quit command to exit the program
         """
         return True
+
     def do_EOF(self, line):
         """EOF exit the program
         """
         print("")
         return True
+
     def emptyline(self):
         """Ignore empty lines
         """
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
