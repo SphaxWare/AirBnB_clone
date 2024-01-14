da10985f160e6c0ca52e1bc30f264aa647bee2ce
Sphax <itssphax123@gmail.com>
2024-01-14 12:48:00 -0800

    quick fix

--- console.py
+++ console.py
@@ -6,6 +6,11 @@ import cmd
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 class HBNBCommand(cmd.Cmd):
@@ -19,7 +24,7 @@ class HBNBCommand(cmd.Cmd):
             'City': City,
             'Place': Place,
             'Review': Review,
-            'State': State,
+            'State': State
             }
 
     def do_quit(self, line):
